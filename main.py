3661bfccd690af9c4d143b4a0430c0afab86f192
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2023-03-13 17:56:26 +0300

    initial functions

---
+++ main.py
@@ -0,0 +1,57 @@
+import telebot
+from utils import storage
+from decouple import config
+
+bot = telebot.TeleBot(config('token'))
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    res = storage.add_new_user(message)
+    if res == 0:
+       bot.send_message(message.chat.id, 'Привет, ты уже есть в базе')
+    elif res == 1:
+         bot.send_message(message.chat.id, 'Привет, добавил тебя в базу')  
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    answer = ''
+
+    if message.text == 'pwd':
+        answer = storage.pwd(message)
+    elif 'mkdir ' in message.text and len(message.text.split(' ')) == 2:
+        storage.mkdir(message)
+        answer = 'Создал папку'
+    elif 'cd ' in message.text and len(message.text.split(' ')) == 2:
+        res = storage.cd(message)
+        if res == -1:
+            answer = 'Нет такой папки'
+        elif res == -2:
+            answer = 'Несколько папок с таким именем'
+        else:
+            answer = 'Перешел в папку'
+    elif message.text == 'ls':
+        res = storage.ls(message)
+
+        if (len(res) == 0):
+            answer = 'Папка пуста'
+            return 0
+
+        for i in res:
+            answer += i[2] + ' '
+
+    bot.send_message(message.chat.id, answer)
+
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+    # получаем информацию о файле
+    file_info = bot.get_file(message.document.file_id)
+    file_url = 'https://api.telegram.org/file/bot{}/{}'.format(bot.token, file_info.file_path)
+    file_name = message.document.file_name
+
+    # выводим информацию на экран
+    print('Получен файл:')
+    print('Ссылка: {}'.format(file_url))
+    print('Название файла: {}'.format(file_name))
+
+
+bot.polling(none_stop=True)
---
+++ utils/storage.py
@@ -0,0 +1,103 @@
+import psycopg2
+
+def ls(message):
+    conn = psycopg2.connect(host="localhost", port=5432, database="postgres", user="alechh", password="123")
+    cur = conn.cursor()
+
+    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = cur.fetchall()[0]
+
+    cur.execute("SELECT * FROM folders WHERE parent_folder_id = %s", (current_directory))
+    res = cur.fetchall()
+
+    conn.close()
+
+    return res
+
+
+def cd(message):
+    conn = psycopg2.connect(host="localhost", port=5432, database="postgres", user="alechh", password="123")
+    cur = conn.cursor()
+
+    new_folder_name = message.text.split(' ')[1]
+
+    if (new_folder_name == '..'):
+        cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+        current_directory = cur.fetchall()[0]
+
+        cur.execute("SELECT parent_folder_id FROM folders WHERE folder_id = %s", (current_directory))
+        parent_folder = cur.fetchall()[0]
+
+        cur.execute("UPDATE user_directories SET current_directory = %s WHERE user_id = %s", (parent_folder, message.from_user.id))
+        conn.commit()
+        conn.close()
+        return 0
+
+    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+
+    current_directory = cur.fetchall()[0]
+
+    cur.execute("SELECT * FROM folders WHERE folder_name = %s and parent_folder_id = %s", (new_folder_name, current_directory))
+
+    res = cur.fetchall()
+
+    if len(res) == 0:
+        conn.close()
+        return -1
+    elif len(res) == 1:
+        cur.execute("UPDATE user_directories SET current_directory = %s WHERE user_id = %s", (res[0][0], message.from_user.id))
+        conn.commit()
+        conn.close()
+        return 0
+    else:
+        conn.close()
+        return -2
+    
+def mkdir(message):
+    conn = psycopg2.connect(host="localhost", port=5432, database="postgres", user="alechh", password="123")
+    cur = conn.cursor()
+
+    new_folder_name = message.text.split(' ')[1]
+
+    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = cur.fetchall()[0]
+
+    cur.execute("INSERT INTO folders (user_id, folder_name, parent_folder_id) VALUES (%s, %s, %s)", (message.from_user.id, new_folder_name, current_directory))
+
+    conn.commit()
+    conn.close()
+
+
+def pwd(message):
+    conn = psycopg2.connect(host="localhost", port=5432, database="postgres", user="alechh", password="123")
+    cur = conn.cursor()
+
+    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = cur.fetchall()[0]
+
+    cur.execute("SELECT * FROM folders WHERE folder_id = %s", (current_directory))
+    res = cur.fetchall()
+    res_tuple = res[0]
+    
+    current_directory_name = res_tuple[2]
+
+    conn.close()
+
+    return current_directory_name
+
+def add_new_user(message):
+    conn = psycopg2.connect(host="localhost", port=5432, database="postgres", user="alechh", password="123")
+    cur = conn.cursor()
+
+    # check if the user exists in table users
+    cur.execute("SELECT * FROM users WHERE user_id = %s", (message.from_user.id,))
+
+    # if not, add the user to the table
+    if not cur.fetchone():
+        cur.execute("INSERT INTO users (user_id, username) VALUES (%s, %s)", (message.from_user.id, message.from_user.username))
+        conn.commit()
+        conn.close()
+        return 1
+    else:
+        conn.close()
+        return 0
