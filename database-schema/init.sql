4f99973cd16163492b6670e0ae6f94fe3fd7b7f8
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2023-04-01 23:16:40 +0300

    TEXT field for file_id and folder_id

--- database-schema/init.sql
+++ database-schema/init.sql
@@ -6,34 +6,34 @@ CREATE TABLE users (
 );
 
 CREATE TABLE files (
-  file_id INTEGER PRIMARY KEY,
+  file_id TEXT PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
   file_name VARCHAR(255),
   file_url VARCHAR(255)
 );
 
 CREATE TABLE folders (
-  folder_id SERIAL PRIMARY KEY,
+  folder_id TEXT PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
   folder_name VARCHAR(255),
-  parent_folder_id INTEGER REFERENCES folders(folder_id) ON DELETE CASCADE
+  parent_folder_id TEXT REFERENCES folders(folder_id) ON DELETE CASCADE
 );
 
 CREATE TABLE folder_files (
-  folder_id INTEGER REFERENCES folders(folder_id) ON DELETE CASCADE,
-  file_id INTEGER REFERENCES files(file_id) ON DELETE CASCADE
+  folder_id TEXT REFERENCES folders(folder_id) ON DELETE CASCADE,
+  file_id TEXT REFERENCES files(file_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_directories (
     user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
-    current_directory INTEGER REFERENCES folders(folder_id) ON DELETE CASCADE
+    current_directory TEXT REFERENCES folders(folder_id) ON DELETE CASCADE
 );
 
 CREATE OR REPLACE FUNCTION create_user_folder()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO folders (user_id, folder_name, parent_folder_id)
-  VALUES (NEW.user_id, 'home', NULL);
+  INSERT INTO folders (folder_id, user_id, folder_name, parent_folder_id)
+  VALUES (NEW.user_id || '_home', NEW.user_id, 'home', NULL);
 
   INSERT INTO user_directories (user_id, current_directory)
   VALUES (NEW.user_id, (SELECT folder_id FROM folders WHERE user_id = NEW.user_id AND folder_name = 'home'));
