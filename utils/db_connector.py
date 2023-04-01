bd9e5a5b31193e42d963390ae41b434d87963523
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2023-04-01 23:15:52 +0300

    separate class for database connector

---
+++ utils/db_connector.py
@@ -0,0 +1,40 @@
+import psycopg2
+
+class DB_Connector:
+    def __init__(self, host, port, user, password, database):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = self.connect()
+        print("Connected to database on {}:{}".format(self.host, self.port))
+    
+    def __del__(self):
+        print("Closing connection to database")
+        self.disconnect()
+
+    def connect(self):
+        try:
+            conn = psycopg2.connect(host=self.host, port=self.port, 
+                                database=self.database, user=self.user, 
+                                password=self.password)
+            return conn
+        except:
+            print("Failed to connect to database on {}:{}".format(self.host, self.port))
+
+    def disconnect(self):
+        try:
+            self.connection.close()
+        except:
+            print("Failed to disconnect from  database on {}:{}".format(self.host, self.port))
+
+    def execute(self, query, params=None):
+        try:
+            cur = self.connection.cursor()
+            cur.execute(query, params)
+            self.connection.commit()
+            return cur.fetchall()
+        except:
+            print("Failed to execute query")
+            return []
--- utils/storage.py
+++ utils/storage.py
@@ -1,15 +1,10 @@
-import psycopg2
+from utils.db_connector import DB_Connector
 from decouple import config
 
 def check_user(message):
-    conn = psycopg2.connect(host=config("db_host"), port=config("db_port"), database=config("db_name"), user=config("db_user"), password=config("db_pass"))
-    cur = conn.cursor()
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
-    cur.execute("SELECT * FROM users WHERE user_id = %s", (message.from_user.id,))
-
-    res = cur.fetchall()
-
-    conn.close()
+    res = db.execute("SELECT * FROM users WHERE user_id = %s", (message.from_user.id,))
 
     if len(res) == 0:
         return False
@@ -17,152 +12,157 @@ def check_user(message):
         return True
 
 def ls(message):
-    conn = psycopg2.connect(host=config("db_host"), port=config("db_port"), database=config("db_name"), user=config("db_user"), password=config("db_pass"))
-    cur = conn.cursor()
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
-    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
 
-    current_directory = cur.fetchall()[0]
+    current_directory = res[0]
 
-    cur.execute("SELECT * FROM folders WHERE parent_folder_id = %s", (current_directory))
-    folders = cur.fetchall()
+    folders = db.execute("SELECT * FROM folders WHERE parent_folder_id = %s", (current_directory))
 
     folders_answer = ''
 
     for folder in folders:
         folders_answer += folder[2] + ' '
 
-    cur.execute("SELECT file_id FROM folder_files WHERE folder_id = %s", (current_directory))
-    files = cur.fetchall()
+    files = db.execute("SELECT file_id FROM folder_files WHERE folder_id = %s", (current_directory))
 
     files_answer = ''
 
     for file in files:
-        cur.execute("SELECT file_name FROM files WHERE file_id = %s", (file[0],))
-        file_name = cur.fetchall()[0][0]
+        res = db.execute("SELECT file_name FROM files WHERE file_id = %s", (file[0],))
+        file_name = res[0][0]
         files_answer += file_name + ' '
-
-    conn.close()
 
     return folders_answer, files_answer
 
 
 def cd(message):
-    conn = psycopg2.connect(host=config("db_host"), port=config("db_port"), database=config("db_name"), user=config("db_user"), password=config("db_pass"))
-    cur = conn.cursor()
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
     new_folder_name = message.text.split(' ')[1]
 
     if (new_folder_name == '..'):
-        cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
-        current_directory = cur.fetchall()[0]
+        res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+        current_directory = res[0]
 
-        cur.execute("SELECT parent_folder_id FROM folders WHERE folder_id = %s", (current_directory))
-        parent_folder = cur.fetchall()[0]
+        res = db.execute("SELECT parent_folder_id FROM folders WHERE folder_id = %s", (current_directory))
+        parent_folder = res[0]
 
         if parent_folder[0] == None:
-            conn.close()
             return -3
 
-        cur.execute("UPDATE user_directories SET current_directory = %s WHERE user_id = %s", (parent_folder, message.from_user.id))
-        conn.commit()
-        conn.close()
+        db.execute("UPDATE user_directories SET current_directory = %s WHERE user_id = %s", (parent_folder, message.from_user.id))
         return 0
 
-    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
 
-    current_directory = cur.fetchall()[0]
+    current_directory = res[0]
 
-    cur.execute("SELECT * FROM folders WHERE folder_name = %s and parent_folder_id = %s", (new_folder_name, current_directory))
-
-    res = cur.fetchall()
+    res = db.execute("SELECT * FROM folders WHERE folder_name = %s and parent_folder_id = %s", (new_folder_name, current_directory))
 
     if len(res) == 0:
-        conn.close()
         return -1
     elif len(res) == 1:
-        cur.execute("UPDATE user_directories SET current_directory = %s WHERE user_id = %s", (res[0][0], message.from_user.id))
-        conn.commit()
-        conn.close()
+        db.execute("UPDATE user_directories SET current_directory = %s WHERE user_id = %s", (res[0][0], message.from_user.id))
         return 0
     else:
-        conn.close()
         return -2
     
 def mkdir(message):
-    conn = psycopg2.connect(host=config("db_host"), port=config("db_port"), database=config("db_name"), user=config("db_user"), password=config("db_pass"))
-    cur = conn.cursor()
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
     new_folder_name = message.text.split(' ')[1]
 
-    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
-    current_directory = cur.fetchall()[0]
+    res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = res[0][0]
 
-    cur.execute("INSERT INTO folders (user_id, folder_name, parent_folder_id) VALUES (%s, %s, %s)", (message.from_user.id, new_folder_name, current_directory))
+    folder_id = (str) (hash((str) (message.from_user.id) + new_folder_name + current_directory))
 
-    conn.commit()
-    conn.close()
+    db.execute("INSERT INTO folders (folder_id, user_id, folder_name, parent_folder_id) VALUES (%s, %s, %s, %s)", (folder_id, message.from_user.id, new_folder_name, current_directory))
 
 
 def pwd(message):
-    conn = psycopg2.connect(host=config("db_host"), port=config("db_port"), database=config("db_name"), user=config("db_user"), password=config("db_pass"))
-    cur = conn.cursor()
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
-    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
-    current_directory = cur.fetchall()[0]
+    res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = res[0]
 
-    cur.execute("SELECT * FROM folders WHERE folder_id = %s", (current_directory))
-    res = cur.fetchall()
+    res = db.execute("SELECT * FROM folders WHERE folder_id = %s", (current_directory))
     res_tuple = res[0]
     
     current_directory_name = res_tuple[2]
 
     while res_tuple[3] != None:
-        cur.execute("SELECT * FROM folders WHERE folder_id = %s", (res_tuple[3],))
-        res = cur.fetchall()
+        res = db.execute("SELECT * FROM folders WHERE folder_id = %s", (res_tuple[3],))
         res_tuple = res[0]
         current_directory_name = res_tuple[2] + '/' + current_directory_name
-
-    conn.close()
 
     return "/" + current_directory_name
 
 def add_new_user(message):
-    conn = psycopg2.connect(host=config("db_host"), port=config("db_port"), database=config("db_name"), user=config("db_user"), password=config("db_pass"))
-    cur = conn.cursor()
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
     # check if the user exists in table users
-    cur.execute("SELECT * FROM users WHERE user_id = %s", (message.from_user.id,))
+    res = db.execute("SELECT * FROM users WHERE user_id = %s", (message.from_user.id,))
 
     # if not, add the user to the table
-    if not cur.fetchone():
-        cur.execute("INSERT INTO users (user_id, username) VALUES (%s, %s)", (message.from_user.id, message.from_user.username))
-        conn.commit()
-        conn.close()
+    if len(res) == 0:
+        db.execute("INSERT INTO users (user_id, username) VALUES (%s, %s)", (message.from_user.id, message.from_user.username))
         return 1
     else:
-        conn.close()
         return 0
     
-def save_file(message, file_name, file_url):
-    conn = psycopg2.connect(host=config("db_host"), port=config("db_port"), database=config("db_name"), user=config("db_user"), password=config("db_pass"))
-    cur = conn.cursor()
+def save_file(message, file_id, file_name, file_url):
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
-    cur.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
-    current_directory = cur.fetchall()[0]
+    res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = res[0]
 
-    cur.execute("SELECT * FROM folder_files WHERE folder_id = %s", (current_directory))
-    number_of_files = len(cur.fetchall())
+    db.execute("INSERT INTO files (file_id, user_id, file_name, file_url) VALUES (%s, %s, %s, %s)", (file_id, message.from_user.id, file_name, file_url))
 
-    cur.execute("INSERT INTO files (file_id, user_id, file_name, file_url) VALUES (%s, %s, %s, %s)", (number_of_files + 1, message.from_user.id, file_name, file_url))
+    db.execute("INSERT INTO folder_files (folder_id, file_id) VALUES (%s, %s)", (current_directory, file_id))
 
-    cur.execute("SELECT file_id FROM files WHERE file_name = %s and file_url = %s", (file_name, file_url))
+    return 0
 
-    file_id = cur.fetchall()[0]
+def rm_file(message):
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
 
-    cur.execute("INSERT INTO folder_files (folder_id, file_id) VALUES (%s, %s)", (current_directory, file_id))
+    file_name = message.text.split(' ')[1]
 
-    conn.commit()
-    conn.close()
+    res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = res[0]
+
+    files_in_current_dir = db.execute ("SELECT file_id FROM folder_files WHERE folder_id = %s", (current_directory,))
+
+    res = db.execute("SELECT file_id FROM files WHERE file_name = %s AND user_id = %s", (file_name, message.from_user.id))
+    if len(res) == 0:
+        return -1
+    
+    file_id = res[0]
+
+    if file_id not in files_in_current_dir:
+        return -1
+
+    db.execute("DELETE FROM folder_files WHERE folder_id = %s and file_id = %s", (current_directory, file_id))
+    db.execute("DELETE FROM files WHERE file_id = %s AND user_id = %s", (file_id, message.from_user.id))
+    return 0
+
+def rm_folder(message):
+    db = DB_Connector(config("db_host"), config("db_port"), config("db_user"), config("db_pass"), config("db_name"))
+
+    folder_name = message.text.split(' ')[1]
+
+    res = db.execute("SELECT current_directory FROM user_directories WHERE user_id = %s", (message.from_user.id,))
+    current_directory = res[0]
+
+    res = db.execute("SELECT folder_id FROM folders WHERE folder_name = %s and parent_folder_id = %s", (folder_name, current_directory))
+    folder_id = ''
+    try:
+        folder_id = res[0]
+    except:
+        return -1
+
+    db.execute("DELETE FROM folders WHERE folder_id = %s", (folder_id))
 
     return 0
