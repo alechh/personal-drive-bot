7a80adcb2288117a1767c71abed21a83bde225f6
Aleksandr Semenov <alex.sergeevich1348@gmail.com>
2023-04-22 16:58:09 +0300

    remove unused import in test_connection.py

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -4,8 +4,6 @@ parent_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_folder)
 
 from utils.db_connector import DB_Connector
-from decouple import config
-import pytest
 
 def test_db_connector():
     db = DB_Connector("localhost", 5432, "alechh", "123", "postgres")
